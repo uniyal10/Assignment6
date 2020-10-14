278bc3ee151ec1ee3c0a7c9bdc8d0c0c69ecabda
sudhanshu uniyal <sudhanshu.uniyal@sourcefuse.com>
2020-10-14 12:17:41 +0530

    curd operation add with user database

---
+++ src/models/user.model.ts
@@ -0,0 +1,62 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstname: string;
+
+  @property({
+    type: 'string',
+  })
+  middlename?: string;
+
+  @property({
+    type: 'string',
+  })
+  lastname?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  phonenumber: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  role: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
